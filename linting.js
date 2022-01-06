a79f65e264574d161542cdc13086fe2c638fee6f
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-06 15:39:59 -0500

    Create linting.js

---
+++ linting.js
@@ -0,0 +1,16 @@
+// linters
+  // help your code be free of errors and vulnerabilities
+  // automatically fix syntax errors, style inconsistencies, missing semicolons, security breaches
+  
+
+// Examples:
+  // ESLINT - for JS
+  // JSHINT 0 for JS
+  // Stylelint - for css
+
+
+// different than a build tool because...
+  // linting happens AS you write your code, wheras build tools are applied only when you want to preview or deploy your code
+
+
+https://eslint.org/docs/user-guide/getting-started
