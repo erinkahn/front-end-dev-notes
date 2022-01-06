9f0f448d22846c3f32319872d12f0822d4587b8a
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-06 16:00:30 -0500

    Create vscodeShortcuts.js

---
+++ vscodeShortcuts.js
@@ -0,0 +1,27 @@
+// vscode shortcuts
+
+// Delete a line: shift+command+k
+
+// Move a line up: option+up (no selection necessary)
+
+// Move a line down: option+down (no selection necessary)
+
+// Copy a line up: shift+option+up
+
+// Copy a line down: shift+option+down
+
+// Find the next match: command+d (select item to match)
+
+// Move to the end of a line: command+right
+
+// Move to the beginning of a line: command+left
+
+// Move to the end of a word: option+right
+
+// Move to the beginning of a word: option+left
+
+// Save a file: command+s
+
+// Select all: command+a
+
+// Open an in-editor terminal: ctrl+`
