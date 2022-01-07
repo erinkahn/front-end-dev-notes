d42d5ec471b89407e66e0037aee26f6011ba7863
Erin Kahn Smith <erinnicolekahn@gmail.com>
2022-01-07 10:09:41 -0500

    Update webpack.js

--- webpack.js
+++ webpack.js
@@ -13,7 +13,7 @@
    // 3. make an entry point where the main bundle file will be
       // the default entry point is index.js in a src folder, but can be changed inside the webpack.config.js file (see below)
       // if there's no folder already, create a src folder and inside create a index.js file that is empty
-      // after changes are made and files are saved, the files inside src will now compile to a dist folder 
+      // after changes are made and files are saved, the files inside src will now compile to a dist folder dist/main.js
   
    // 4. add scripts to package.json to watch webpack to look for updates to our files and rebuild if changes occur
       scripts": {
@@ -34,7 +34,8 @@
        // they will be removed when the mode is set to 'production'
         
 
-// to change the entry point & exit point
+// to change the entry point & exit point   
+       // note: if you change the entry point but don't change the exit point, it will always compile to an exit point of dist/main.js
        // go to webpack.config.js
        // say you want to create an outpoint folder called built and inside that a file called fast.js, you would write:
         const path = require('path');
