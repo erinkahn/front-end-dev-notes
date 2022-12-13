44087d6d0d443b5cf0c4fbccd117ffbb6b4949d5
Erin N Kahn <erinnicolekahn@gmail.com>
2022-12-13 11:07:56 -0600

    Update asynchronous.js

--- asynchronous.js
+++ asynchronous.js
@@ -33,3 +33,64 @@
 // synchronous OR asynchronous?
     // if you want to run a simple app with sequential linear tasks like process data, then use synchronous programming
     // if you want to run a app that calls for responding to events like a GUI program, then use asynchronous programming
+
+
+const useAsync = async () => {
+    let resolvedVal = await myPromise(); // await pauses the execution of async function until the promise is resolved then returns the promise
+    console.log(resolvedVal);
+}
+
+useAsync();
+
+
+// Example:
+
+    // 1.promise function
+    const brainstormDinner = () => {
+      return new Promise((resolve, reject) => {
+      console.log(`I have to decide what's for dinner...`)
+      setTimeout(() => {
+        console.log('Should I make salad...?');
+        setTimeout(() => {
+          console.log('Should I make ramen...?');
+          setTimeout(() => {
+            console.log('Should I make eggs...?');
+            setTimeout(() => {
+              console.log('Should I make chicken...?');
+              resolve('beans');
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    });
+    };
+
+    // 2a. DONT USE THIS: native promise version:
+    function nativePromiseDinner() {
+      brainstormDinner().then((meal) => {
+          console.log(`I'm going to make ${meal} for dinner.`);
+      });
+    }
+
+    // 2b. USE THIS: async/await version:
+    async function announceDinner() {
+      // Write your code below:
+      let dinnerChoice = await nativePromiseDinner();
+      console.log(dinnerChoice)
+
+    }
+
+    // 3. call the function
+    announceDinner()
+
+    // returns the following:
+        //     I have to decide what's for dinner...
+        //     undefined
+        //     Should I make salad...?
+        //     Should I make ramen...?
+        //     Should I make eggs...?
+        //     Should I make chicken...?
+        //     I'm going to make beans for dinner.
+
+
+    
